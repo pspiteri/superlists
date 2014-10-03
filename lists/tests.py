d80362d77de8ad5ea9958ca9bec348f89aee0789
Paul Spiteri <paul.spiteri@bigredzed.com.au>
2014-10-03 21:08:16 +1000

    More FT

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,6 @@ from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
-
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -15,4 +14,18 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
